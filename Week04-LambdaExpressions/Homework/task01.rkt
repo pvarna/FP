a5764c665efc84b166dd343b678c90a982069044
pvarna <peter.kolev2001@gmail.com>
2021-11-01 10:29:14 +0200

    rename a function

--- Week04-LambdaExpressions/Homework/task01.rkt
+++ Week04-LambdaExpressions/Homework/task01.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(define (big-function f y)
+(define (upper-bound f y)
   (λ (x)
     (if (> x (f x))
         y
@@ -9,6 +9,6 @@
     )
   )
 
-((big-function (λ (x) (* 2 x)) 100) 50)
-((big-function (λ (x) (* 2 x)) 100.236) 500.002)
-((big-function identity 1.001) 1.001)
+((upper-bound (λ (x) (* 2 x)) 100) 50)
+((upper-bound (λ (x) (* 2 x)) 100.236) 500.002)
+((upper-bound identity 1.001) 1.001)
